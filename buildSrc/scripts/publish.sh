cc52767f8335891cb650ff8f0280a4a5f713e2c9
Kusaanko <39370373+kusaanko@users.noreply.github.com>
2023-08-27 15:35:21 +0900

    Change maven url to local repository

--- buildSrc/scripts/publish.sh
+++ buildSrc/scripts/publish.sh
@@ -5,22 +5,22 @@ cd $BASEDIR/../.. || exit
 
 echo '> Publishing Modules...'
 echo '>> Module [imgui-app]'
-./gradlew imgui-app:publishImguiPublicationToMavenCentralRepository
+./gradlew imgui-app:publish
 echo '>> Module [imgui-lwjgl3]'
-./gradlew imgui-lwjgl3:publishImguiPublicationToMavenCentralRepository
+./gradlew imgui-lwjgl3:publish
 echo '>> Module [imgui-binding]'
-./gradlew imgui-binding:publishImguiPublicationToMavenCentralRepository
+./gradlew imgui-binding:publish
 
 echo '> Publishing Natives...'
 echo '>> Natives: [windows, freetype]'
-./gradlew imgui-binding-natives:publishImguiPublicationToMavenCentralRepository -PdeployType=windows -Pfreetype=true
+./gradlew imgui-binding-natives:publish -PdeployType=windows -Pfreetype=true
 echo '>> Natives: [windows]'
-./gradlew imgui-binding-natives:publishImguiPublicationToMavenCentralRepository -PdeployType=windows -Pfreetype=false
+./gradlew imgui-binding-natives:publish -PdeployType=windows -Pfreetype=false
 echo '>> Natives: [linux, freetype]'
-./gradlew imgui-binding-natives:publishImguiPublicationToMavenCentralRepository -PdeployType=linux -Pfreetype=true
+./gradlew imgui-binding-natives:publish -PdeployType=linux -Pfreetype=true
 echo '>> Natives: [linux]'
-./gradlew imgui-binding-natives:publishImguiPublicationToMavenCentralRepository -PdeployType=linux -Pfreetype=false
+./gradlew imgui-binding-natives:publish -PdeployType=linux -Pfreetype=false
 echo '>> Natives: [macOS, freetype]'
-./gradlew imgui-binding-natives:publishImguiPublicationToMavenCentralRepository -PdeployType=macos -Pfreetype=true
+./gradlew imgui-binding-natives:publish -PdeployType=macos -Pfreetype=true
 echo '>> Natives: [macOS]'
-./gradlew imgui-binding-natives:publishImguiPublicationToMavenCentralRepository -PdeployType=macos -Pfreetype=false
+./gradlew imgui-binding-natives:publish -PdeployType=macos -Pfreetype=false
